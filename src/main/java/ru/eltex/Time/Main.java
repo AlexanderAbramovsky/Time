3907da7f79f1b41a57982eae776c6f9327fc63a4
Alexandr Abramovsky <sahan.abr@yandex.ru>
2019-04-23 16:20:12 +0700

    Подключение базы данных к серверу java

---
+++ src/main/java/ru/eltex/Time/Main.java
@@ -0,0 +1,12 @@
+package ru.eltex.Time;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ControllerMySQL test = new ControllerMySQL();
+
+        test.start();
+
+        System.out.println("I JOB ПРИВЕТ МИР");
+    }
+}
